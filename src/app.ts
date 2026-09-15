71481db96c7efb5693f787cb23cbf1ab515cbde4
favoreto.ryan@gmail.com <favoreto.ryan@gmail.com>
2026-09-15 10:42:50 -0300

    jdhajshdad

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,61 @@
 // Importa a biblioteca Express e também o tipo Express
 // O Express será utilizado para criar o servidor web
 import express from "express";
-import type { Express } from "express";
+import type { Express, Request, Response } from "express";
+//importa a classe player do arquivo Player.ts
+import { Player } from "./models/Player.js";
 
-// Cria uma aplicação Express
-// A função express() devolve um objeto que representa o servidor da aplicação
 const app: Express = express();
 
+// Midleware para permitir que o servidor entenda requissições com corppo em JSON
+app.use(express.json());
+
+// Cria uma aplicação Express
+// A função express() devolve um objeto que representa o servidor da aplicaçã
 // Define a porta onde o servidor ficará disponível
 // Neste caso, o servidor poderá ser acessado pela porta 8081
 const PORT: number = 8081;
+
+//criampps (instanciamos) um novo jogador chamado hero com 10 de saude e nivel 1
+let player1: Player = new Player("Hero", 100, 5);
+
+app.get("/player", (req: Request, res: Response) => {
+    res.json({
+        message: "Informações do Jogador",
+        player:player1,
+    });
+});
+
+app.post("/player/attack", (req: Request, res: Response) => {
+    const attackMessage = player1.attack();
+    res.json({
+        message: attackMessage,
+    });
+});
+
+// Rota POST para o jogador receber dano
+// Q
+app.post("/player/take-damage", (req: Request, res: Response) => {
+    const { damage } = req.body;
+    const damageMessage = player1.takeDamage(damage);
+    // Retorna uma resosta JSON com a mensagem do dano
+    //para o cliente que fez a requisição
+    res.json({
+        //Retorna a mensagem do dano recebido
+        action: damageMessage,
+        // Retorna a saude atual do jogador
+        currentHealth: player1.health,
+        // Retorna o nível do jogador
+        currentLevel: player1.level 
+    });
+});
 
 // Inicializa o servidor utilizando a porta definida
 // O método listen() faz o servidor começar a "escutar" requisições HTTP
 app.listen(PORT, () => {
 console.log(`Servidor rodando em http://localhost:${PORT}`);
+console.log("Rotas disponiveis:");
+console.log(`GET http://localhost:${PORT}/player - obter informações do jogador`);
+console.log(`POST http://localhost:${PORT}/player/attack - Jogador realiza um ataque`);
+console.log(`POST http://localhost:${PORT}/player/take-damage - Jogador recebe dano`);
 });
---
+++ src/models/Player.ts
@@ -0,0 +1,35 @@
+// A palavra "class" define que estamos criando um molde.
+// A palavra "export" permite que esse arquivo seja usado por outros arquivos (como o appp.ts).
+
+export class Player {
+    public name: string; // O nome do jogador (texto)
+    public health: number; // A saude do jogador (número)
+    public level: number; // O nível do jogador (número)
+
+    // Construtores (O construtor é um método especial que executado automaticamente quando a classe é instanciada uma única vez)
+    constructor(name: string, health: number = 100, level: number = 1) {
+        // A palavra "this" faz referência aealth: própria classe, ou seja
+        // "Pegue o atrributo 'name' da classe Player e atribua o valor do parametro 'name' a ele
+
+        this.name = name;
+        this.health = health;
+        this.level = level;
+    }
+
+    // Métodos (Comportamentos da classe)
+    // Métodos são as funções que a classe ppode executras, ou seja, sao os comportamentos da classe
+    public attack(): string {
+        const damage = this.level * 10;
+        return `${this.name} atacou e causou ${damage} de dano`;
+    }
+    //O método de take damage
+    public takeDamage(amount: number): string {
+        this.health -= amount;
+        if (this.health < 0) {
+            this.health = 0;
+            return `${this.name} foi derrotado!!!!!`;
+        }
+
+        return `${this.name} recebeu ${amount} de dano e agora tem ${this.health} de saúde.`;
+    }
+}
